bf0124c658349a6fa6355f7bd09987dc6f4da1e2
Toktam <ttmohebbi@gmail.com>
2020-10-07 20:24:45 +0000

    testing the createTweetElement function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -41,34 +41,22 @@ const createTweetElement = function(tweetObject) {
   return $tweet;
 };
 
-console.log(createTweetElement(sampleTweetObject));
 
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+      "user": {
+      "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+  },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+  },
+  "created_at": 1461116232227
+}
 
+const $tweet = createTweetElement(tweetData);
 
-
-
-
-
-
-
-
-
-
-// // Test / driver code (temporary). Eventually will get this from the server.
-// const tweetData = {
-//       "user": {
-//       "name": "Newton",
-//     "avatars": "https://i.imgur.com/73hZDYK.png",
-//     "handle": "@SirIsaac"
-//   },
-//   "content": {
-//       "text": "If I have seen further it is by standing on the shoulders of giants"
-//   },
-//   "created_at": 1461116232227
-// }
-
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+// Test / driver code (temporary)
+console.log($tweet); // to see what it looks like
+$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
