e8c9d0a76204e3fbde18299277803ecb321784ff
Toktam <ttmohebbi@gmail.com>
2020-10-08 00:40:45 +0000

    added renderTweets function

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,59 +4,72 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const sampleTweetObject = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+
+// Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+]
+
+const renderTweets = function(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+
+  for (const tweet of tweets) {
+    const $tweet = createTweetElement(tweet);
+    $(".tweets-container").append($tweet);
+  }
+
 }
 
-
-// use dot notation instead of bracket notation for code readability
 
 const createTweetElement = function(tweetObject) {
-
-  const $tweet = `<article class="tweet-article">
-  <header class="tweet-header">
-    <img class="tweet-avatar" src="${tweetObject.user.avatars}">
-      <h3 class>${tweetObject.user.name}</h3>
-      <h3 class="tweet-handle">${tweetObject.user.handle}</h3>
-</header>
-    <h3>${tweetObject.content.text}</h3>
-    <footer>
-      <span class="tweet-date">${tweetObject.created_at}</span>
-      <span class="tweet-icons">
-        <div><i class="far fa-flag"></i></div>
-        <div><i class="fas fa-retweet"></i></div>
-        <div><i class="far fa-heart"></i></div>
-      </span>
-    </footer>
-</article>`;
-
+  let $tweet = `<article class="tweet-article">
+    <header class="tweet-header">
+      <img class="tweet-avatar" src="${tweetObject.user.avatars}">
+        <h3 class>${tweetObject.user.name}</h3>
+        <h3 class="tweet-handle">${tweetObject.user.handle}</h3>
+    </header>
+      <h3>${tweetObject.content.text}</h3>
+      <footer>
+        <span class="tweet-date">${tweetObject.created_at}</span>
+        <span class="tweet-icons">
+          <div><i class="far fa-flag"></i></div>
+          <div><i class="fas fa-retweet"></i></div>
+          <div><i class="far fa-heart"></i></div>
+        </span>
+      </footer>
+    </article>`;
   return $tweet;
-};
-
-
-// Test / driver code (temporary). Eventually will get this from the server.
-const tweetData = {
-      "user": {
-      "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
-  },
-  "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-  },
-  "created_at": 1461116232227
 }
 
-const $tweet = createTweetElement(tweetData);
 
-// Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
-$('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+$('document').ready(function() {
+  renderTweets(data);
+});
+
+
+
+
